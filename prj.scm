28ecc0de21eab4e4796ae401b447f77ff4224805
Hugo Hörnquist <hugo@lysator.liu.se>
2020-12-28 00:52:21 +0100

    Add prj-file parser.

---
+++ prj.scm
@@ -0,0 +1,43 @@
+(define-module (prj)
+  :use-module (ice-9 peg)
+  :use-module (ice-9 match)
+  :export (parse-prj-string))
+
+(define-peg-pattern string all
+  (and (ignore "\"")
+       (* (and (not-followed-by "\"") peg-any))
+       (ignore "\"")))
+
+
+(define-peg-pattern digit body
+  (range #\0 #\9)
+  )
+
+(define-peg-pattern integer body
+  (+ digit))
+
+(define-peg-pattern number all
+  (and integer "." integer))
+
+(define-peg-pattern record all
+  (and (+ (range #\A #\Z))
+       (ignore "[")
+       string
+       (* (and (ignore ",") (or record string number)))
+       (ignore "]")))
+
+;; (peg:tree (match-pattern record str))
+
+(define (parse-tree tree)
+  (match tree
+    (('string s) s)
+    (('number s) (string->number s))
+    (('record name body ...)
+     `(,(string->symbol name)
+       ,@(map parse-tree (keyword-flatten '(string record number)
+                                      body))))))
+
+
+
+(define (parse-prj-string string)
+  (parse-tree (peg:tree (match-pattern record string))))
